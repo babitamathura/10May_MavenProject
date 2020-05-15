51c69623e1378fd5e88f9c0fb8c60e3ea99f9a9b
Babita <babitamathura@gmail.com>
2020-05-15 20:59:49 +0530

    First Commit

---
+++ 10May_MavenProject/src/main/java/E_CommerceApplication/May_MavenProject/App.java
@@ -0,0 +1,13 @@
+package E_CommerceApplication.May_MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
